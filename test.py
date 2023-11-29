236170da959c873d48917aec4158b8c80da94d62
JhangJheWei <sj340821@gmail.com>
2023-11-29 17:04:24 +0800

    線編號的部分已配置完畢

--- test.py
+++ test.py
@@ -31,33 +31,6 @@ for y in range(1,(P_y-1),1):
         number_3+=1
         plt.scatter(x+0.5,y+0.5,s=300,c='y',marker='s')
         plt.text(x+0.5,y+0.5,number_3,fontsize=20,verticalalignment='center',horizontalalignment='center')
-#畫最外圍四方形
-number_4 = 0
-for y in range(1,2,1):
-    for x in range(1,P_x,1):
-        number_4+=1
-        plt.scatter(x,y-0.5,s=300,c='g',marker='s')
-        plt.text(x,y-0.5,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
-        plt.plot([x,x],[y-0.5,y], color='black')#畫線段
-for y in range(1,P_y,1):
-    for x in range(P_x,P_x+1,1):
-        number_4+=1
-        plt.scatter(x-0.5,y,s=300,c='g',marker='s')
-        plt.text(x-0.5,y,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
-        plt.plot([x-0.5,x-1],[y,y], color='black')#畫線段
-for y in range(P_y,P_y+1,1):
-    for x in range(P_x,1,-1):
-        number_4+=1
-        plt.scatter(x-1,y-0.5,s=300,c='g',marker='s')
-        plt.text(x-1,y-0.5,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
-        plt.plot([x-1,x-1],[y-0.5,y-1], color='black')#畫線段
-for y in range(P_y,1,-1):
-    for x in range(1,2,1):
-        number_4+=1
-        plt.scatter(x-0.5,y-1,s=300,c='g',marker='s')
-        plt.text(x-0.5,y-1,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
-        plt.plot([x-0.5,x],[y-1,y-1], color='black')#畫線段
-#############
 #畫黃色方塊的左右線段
 for y in range(1,(P_y-1),1):
     for x in range(1,(P_x-1),1):
@@ -81,29 +54,56 @@ for y in range(1,(P_y-1),1):
         plt.plot([x+0.5,x+1],[y+0.5,y+1], color='black')
 #############
 #線編號
-even_number = []
-odd_number = []
-i = 0
-for count in range(1, total_number*4, 1):
-    if (count % 2) != 0:
-        odd_number.append(count)
-    else:
-        even_number.append(count)
-
-for y in range(1,(P_y-1),1):#畫黃色方塊的左下線段
-    for x in range(1,(P_x-1),1):
-        if i < len(odd_number):
-            plt.text(x+0.2,y+0.2,odd_number[i], color='black')
-            i+=1
-i = 0
-for y in range(1,(P_y-1),1):#畫黃色方塊的右下線段
-    for x in range(1,(P_x-1),1):
-        if i < len(even_number):
-            plt.text(x+0.7,y+0.2,even_number[i], color='black')
-            i+=1
-        else:
-            i = even_number[i-1] + 2
-            plt.text(x+0.7,y+0.2,i, color='black')
-i = 0
+line_count = 1
+for y in np.arange(1,P_y,0.5):
+    for x in np.arange(1,P_x,0.5):
+        if x < P_x-1 and y < P_y-1:
+            plt.text(x+0.2, y+0.25, str(line_count), ha='center', va='center',color='black') #設定斜線字體
+            line_count+=1
+for y in np.arange(1,P_y,0.5):
+    for x in np.arange(1,P_x,1):
+        if x<P_x-1 and y<P_y-1:
+            plt.text(x+0.5, y+0.25, str(line_count), ha='center', va='center',color='black') #設定直線字體
+            line_count+=1
+for y in np.arange(1,P_y,1):
+    for x in np.arange(1,P_x,0.5):
+        if x<P_x-1 and y<P_y-1:
+            plt.text(x+0.25, y+0.5, str(line_count), ha='center', va='center',color='black') #設定橫線字體
+            line_count+=1
+#畫最外圍四方形
+number_4 = 0
+for y in range(1,2,1):
+    for x in range(1,P_x,1):
+        number_4+=1
+        plt.scatter(x,y-0.5,s=300,c='g',marker='s')
+        plt.text(x,y-0.5,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
+        plt.plot([x,x],[y-0.5,y], color='black')#畫線段
+        plt.text(x, y-0.3, str(line_count), ha='center', va='center',color='black') #設定橫線字體
+        line_count+=1
+for y in range(1,P_y,1):
+    for x in range(P_x,P_x+1,1):
+        number_4+=1
+        plt.scatter(x-0.5,y,s=300,c='g',marker='s')
+        plt.text(x-0.5,y,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
+        plt.plot([x-0.5,x-1],[y,y], color='black')#畫線段
+        plt.text(x-0.7, y, str(line_count), ha='center', va='center',color='black') #設定橫線字體
+        line_count+=1
+for y in range(P_y,P_y+1,1):
+    for x in range(P_x,1,-1):
+        number_4+=1
+        plt.scatter(x-1,y-0.5,s=300,c='g',marker='s')
+        plt.text(x-1,y-0.5,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
+        plt.plot([x-1,x-1],[y-0.5,y-1], color='black')#畫線段
+        plt.text(x-1, y-0.8, str(line_count), ha='center', va='center',color='black') #設定橫線字體
+        line_count+=1
+for y in range(P_y,1,-1):
+    for x in range(1,2,1):
+        number_4+=1
+        plt.scatter(x-0.5,y-1,s=300,c='g',marker='s')
+        plt.text(x-0.5,y-1,number_4,fontsize=20,verticalalignment='center',horizontalalignment='center')
+        plt.plot([x-0.5,x],[y-1,y-1], color='black')#畫線段
+        plt.text(x-0.2, y-1, str(line_count), ha='center', va='center',color='black') #設定橫線字體
+        line_count+=1
+#############
 plt.axis("off")
 plt.show()
